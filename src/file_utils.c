30b52fbfcff8e1a31947c580ac04a762adce452a
fairouz623 <fayrouzabrb@gmail.com>
2026-05-08 01:21:09 +0100

    updates

--- src/file_utils.c
+++ src/file_utils.c
@@ -1,3 +1,11 @@
 #include "../include/file_utils.h"
 #include<stdio.h>
 #include<stdlib.h>
+
+int copyBinaryFile(const char* src, const char* dest){
+
+    FILE *fp1;
+    FILE *fp2;
+
+    
+}
--- src/linked_list.c
+++ src/linked_list.c
@@ -1,3 +1,26 @@
 #include "../include/linked_list.h"
 #include<stdio.h>
 #include<stdlib.h>
+
+/* Initialize my single linked list */
+void initList(List* L){
+
+    L -> head = NULL;
+    L -> size = 0;
+
+}
+/* insertation at the beginning of the list */
+int insertBeginning(List* L, int value){
+
+    struct Node* new_node = (struct Node*) malloc (sizeof(struct Node));
+
+    if(new_node == NULL)
+       retrn -1;
+
+    new_node->data = value;
+    new_node->next = L->head;
+    L->head = new_node;
+    L->size++;
+
+    return 0;
+}
